91a8d3e473f8aeb3b84b1f937540d9b713ad3f6b
Himanshusingh038 <Himanshusingh038@gmail.com>
2020-12-01 01:42:21 +0530

    added some file

---
+++ classy.py
@@ -0,0 +1,7 @@
+class Math:
+    notes='This is a class variable'
+    def add(self,n1,n2):
+        print('addition: ',n1+n2)
+    def multiply(selfself,n1,n2):
+        print('multiplication: ',n1*n2)
+
---
+++ run.py
@@ -0,0 +1,143 @@
+from flask import Flask, request
+from  flask import  render_template
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.update(
+
+    SECRET_KEY='him@psql1',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:him@psql1@localhost/catalog_db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+
+db = SQLAlchemy(app) #instance of the class
+
+@app.route('/index')
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask!'
+
+@app.route('/new/')
+def query_string(greeting='hello'):
+    query_val=request.args.get('greeting',greeting)
+    return '<h1> the greeting is :{0}</h1>'.format(query_val)
+
+#now we don't have to use query string ...
+# this is a much more elegent way as url looks simpler and cleaner
+@app.route('/user')
+@app.route('/user/<name>')
+def no_query_strings(name='Himanshu'):
+    return '<h1> hello there! {}</h1>'.format(name)
+# now we will learn how to work with different DATA TYPES
+#STRING
+@app.route('/test/<string:name>')
+def work_string(name):
+    return '</h1> Here is a String: '+name+'</h1>'
+#numbers
+@app.route('/add/<int:num1>/<int:num2>')
+def work_int_add(num1,num2):
+    return '<h1> The Sum is : {}'.format(num1+num2)+'</h1>'
+#float
+@app.route('/divide/<float:n1>/<float:n2>')
+def division(n1,n2):
+    return '<h1> The division is {}'.format(n1/n2)+'</h1>'
+
+
+#render HTML file
+@app.route('/temp')
+def using_template():
+    return render_template('hello.html')
+@app.route('/watch')
+def top_movies():
+    movie_list = ['autopsy of jane doe',
+                  'neon demon',
+                  'ghost in a shell',
+                  'kong: skull island',
+                  'john wick 2',
+                  'spiderman - homecoming']
+
+    return render_template('movies.html',
+                           movies=movie_list,
+                           name='Harry')
+
+# JINJA TEMPLATES 2
+@app.route('/tables')
+def movies_plus():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('table_data.html',
+                           movies=movies_dict,
+                           name='Sally')
+@app.route('/filter')
+def movies_plus1():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('filter_data.html',
+                           movies=movies_dict,
+                           name='Sally')
+@app.route('/macros')
+def movies_plus2():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('using_macros.html', movies=movies_dict)
+
+# PUBLICATION TABLE
+class Publication(db.Model):
+    __tablename__ = 'publication'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'The Publisher is {}'.format(self.name)
+
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False, index=True)
+    author = db.Column(db.String(350))
+    avg_rating = db.Column(db.Float)
+    format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    # ESTABLISH A RELATIONSHIP BETWEEN PUBLICATION AND BOOK TABLES
+    pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+
+    def __init__(self, title, author, avg_rating, book_format, image, num_pages, pub_id):
+        self.title = title
+        self.author = author
+        self.avg_rating = avg_rating
+        self.format = book_format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+
+    def __repr__(self):
+        return '{} by {}'.format(self.title, self.author)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
